b11f9570dc65637dfc3ab57cd826b8635656cc51
Shruti Priya <shruti62442@iitkgp.ac.in>
2018-08-11 10:54:23 +0530

    Mistake in variable rectified

--- src/Vehicle.cpp
+++ src/Vehicle.cpp
@@ -1,26 +1,26 @@
 #include "../include/Vehicle.hpp"
 
-vector<State> nextStates(State n)
+vector<Vehicle::State> Vehicle::nextStates(Vehicle::State n)
 {
-	vector<State> next;
-	State t;
-	float alpha,beta,R;
+	vector<Vehicle::State> next;
+	Vehicle::State t;
+	float alpha,beta,R,d; //alpha=steering angle, beta = turning angle, R=turning radius, d= distanced travelled
 
 	for(alpha=-CAR_MAX_ALPHA; alpha<=CAR_MAX_ALPHA; alpha+=CAR_MAX_ALPHA/2)
 	{
-		beta= (CAR_W/CAR_L)*tan(alpha*PI/180);
+		beta= (d/CAR_L)*tan(alpha*PI/180);
 
 		if(abs(beta)>0.001)
 		{
-			R=CAR_W/beta;
-			t.x=n.x - sin(n.theta*PI/180)*R + sin((n.theta+beta)*PI/180)*R;
-			t.y=n.y + cos(n.theta*PI/180)*R - cos((n.theta+beta)*PI/180)*R;
+			R=d/beta;
+			t.x=n.x - sin(n.theta*PI/180)*R + sin((n.theta*PI/180)+beta)*R;
+			t.y=n.y + cos(n.theta*PI/180)*R - cos((n.theta*PI/180)+beta)*R;
 			t.theta=fmod(n.theta+beta,2*PI);
 		}
 		else
 		{
-			t.x=n.x + CAR_W*cos(n.theta*PI/180);
-			t.y=n.y + CAR_W*sin(n.theta*PI/180);
+			t.x=n.x + d*cos(n.theta*PI/180); // if turning radius is very small we assume that the back tire has moved straight
+			t.y=n.y + d*sin(n.theta*PI/180);
 			t.theta=n.theta;
 		}
 		t.steer_angle=alpha;
